de234a88d89da06de49906d511087e05c79a566f
StonedFlower <63636439+StonedFlower@users.noreply.github.com>
2020-04-16 11:32:18 +0800

    Update PriorityQueueTest.java

--- src/PriorityQueueTest.java
+++ src/PriorityQueueTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
