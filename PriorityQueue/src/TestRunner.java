ce5a2d5546d5210e944010d17833dfe4e706a7d4
StonedFlower <63636439+StonedFlower@users.noreply.github.com>
2020-04-16 13:10:59 +0800

    Update TestRunner.java

--- PriorityQueue/src/TestRunner.java
+++ PriorityQueue/src/TestRunner.java
@@ -3,11 +3,13 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Result result = JUnitCore.runClasses(PriorityQueueTest.class);
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
+            throw failure;
         }
         System.out.println(result.wasSuccessful());
+            
     }
 }
