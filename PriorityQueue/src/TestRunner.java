7857877961cfa8e682303da8e283d45ff19b3e60
StonedFlower <63636439+StonedFlower@users.noreply.github.com>
2020-04-16 13:25:09 +0800

    Update TestRunner.java

--- PriorityQueue/src/TestRunner.java
+++ PriorityQueue/src/TestRunner.java
@@ -7,7 +7,7 @@ public class TestRunner {
         Result result = JUnitCore.runClasses(PriorityQueueTest.class);
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
-            throw new IllegalArgumentException(failure.toString());;
+            throw new IllegalArgumentException(failure.toString());
         }
         System.out.println(result.wasSuccessful());
             
